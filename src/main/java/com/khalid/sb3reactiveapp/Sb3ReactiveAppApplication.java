5130ef96c2d8002bdd5a760848d4ad0f0c307ef0
Khalid <khalid.spy@gmail.com>
2022-08-07 20:15:03 +0530

    Initial Commit

---
+++ src/main/java/com/khalid/sb3reactiveapp/Sb3ReactiveAppApplication.java
@@ -0,0 +1,13 @@
+package com.khalid.sb3reactiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3ReactiveAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb3ReactiveAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khalid/sb3reactiveapp/Sb3ReactiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalid.sb3reactiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3ReactiveAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
